4f4676ab6548e15e95a9c7be8b1d44d8e059ada1
Ibimina Koko <kokoibimina@gmail.com>
2012-08-11 10:17:40 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lost::Application.config.secret_token = 'b884aca535269cd56c5261685d497dd93af9313175a836fceb05084bb35f0e1f80d3d9cee4424d32cbb35ad8704ab91c849314b5f9a185048cbf16c18ea93578'
